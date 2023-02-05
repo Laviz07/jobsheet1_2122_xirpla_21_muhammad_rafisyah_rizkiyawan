55fafdbc39916e495bdd2a94c9c36ec42182cda7
Rafisyah <m.arifin.putrasetia@gmail.com>
2023-02-05 13:15:31 +0700

    update

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -7,8 +7,9 @@ const Footer = () => {
     <p>© 2023 Jajanan Bungur</p>
     <div >
         
-    <ul class="sosmed-foot"><p className='sosmed-text'>Ingin Tetap Terhubung?</p>
-        <li>
+    <ul class="sosmed-foot"><p className='sosmed-text' style={{display:'inline'}}>Ingin Tetap Terhubung?</p>
+        
+        <li className='yutub'>
             <a href='https://www.youtube.com/@samsungindonesia'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--soft-blue)" class="bi bi-youtube" viewBox="0 0 16 16">
                     <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
@@ -43,8 +44,8 @@ const Footer = () => {
             </svg>
             </a>
             </li>
-            <li>
-            <a href='#' className='up'>
+            <li className='up'>
+            <a href='#'>
                 <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--white)" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
                 </svg>
